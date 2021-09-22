adb9fdd886a32b2212c67809a399fbc976eed802
Vetrivelan <62693952+vetri09@users.noreply.github.com>
2021-09-22 20:58:16 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const UserModel = require('../models/UserModel');
-// const bcrypt = require('bcrypt');
-// const dotenv = require('dotenv').config();
-// const jwt = require('jsonwebtoken');
-// const nodemailer = require('nodemailer');
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("<h1>auth</h1>")
 });
