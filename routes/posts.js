c82ca8625232a0df208a352496d8b29baae1e64a
Vetrivelan <62693952+vetri09@users.noreply.github.com>
2021-09-22 20:39:44 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -34,7 +34,7 @@ router.get('/feed/:userId', async(req,res)=>{
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
 // get user post using name
 router.get('/userpost/:userName', async(req,res)=>{
     try {
@@ -44,7 +44,7 @@ router.get('/userpost/:userName', async(req,res)=>{
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
 // get user post
 router.get('/user/userpost/:userId',async(req,res)=>{
     try {
@@ -54,6 +54,40 @@ router.get('/user/userpost/:userId',async(req,res)=>{
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+// post
+// create post
+router.post('/:_id', async(req,res)=>{
+    if(req.body.image) {
+        const imgbbObj = await imgbb.upload(req.body.image);
+        const imageURL = (imgbbObj.data.url).toString();
+        try {
+            const { userId, content } = req.body;
+            const newPost = new PostModel({
+                userId: userId,
+                content: content,
+                image: imageURL
+            });
+            await newPost.save();
+            res.status(200).json("New post added");
+            }
+        catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    else {
+        try {
+            const newPost = new PostModel(req.body);
+            await newPost.save();
+            res.status(200).json("New post added");
+        }
+        catch (error) {
+            res.status(500).json(error);
+        }
+    }
+});
 
+// comment in post
+// update comment
+// delete comment
 module.exports = router;
