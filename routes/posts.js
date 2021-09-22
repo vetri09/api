bc9568b6fda94d785af5f9663585a4ac8c5e3138
Vetrivelan <62693952+vetri09@users.noreply.github.com>
2021-09-22 20:43:41 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -86,7 +86,51 @@ router.post('/:_id', async(req,res)=>{
         }
     }
 });
-
+// put
+// update post
+router.put('/update/:_id', async(req,res)=>{
+    if(req.body.image) {
+        const imgbbObj = await imgbb.upload(req.body.image);
+        req.body.image = (imgbbObj.data.url).toString();
+    }
+    const post = await PostModel.findByIdAndUpdate(req.params._id, {
+            $set:req.body
+        })
+        console.log(req.body)
+        res.status(200).send({
+            post,
+            message:"Post updated"
+        })
+});
+// like || dislike a post
+router.put('/:_id/like', async(req,res)=>{
+    try {
+        const post = await PostModel.findById(req.params._id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("Post liked")
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("Post disliked")
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+// delete post
+router.delete('/:_id', async(req,res)=>{
+    try {
+        const post = await PostModel.findById(req.params._id);
+        if(post.userId === req.body.userId){
+            await post.deleteOne();
+            res.status(200).send({ messages:"Post deleted"})
+        }else{
+            res.status(403).json({messages:"You can delete only your post"})
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 // comment in post
 // update comment
 // delete comment
