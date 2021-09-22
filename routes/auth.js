42a7867c7a4585f58ce590b51fb21b0c763e7ece
Vetrivelan <62693952+vetri09@users.noreply.github.com>
2021-09-22 21:14:02 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -58,5 +58,75 @@ router.get('/confirmtoken/:token', async(req,res)=>{
         res.send(e);
     }
 });
+// user register
+router.post('/register', async(req,res)=>{
+    try {
+        const list = await new UserModel(req.body);
+        list.save((err,user)=>{
+            if(user) {
+                res.status(201).send({
+                    list,
+                    message:"User added"
+                })
+            }
+            else {
+                if(err.code && err.code == 11000) {
+                    console.log(Object.keys(err.keyValue).toString())
+                    const field = Object.keys(err.keyValue).toString();
+                    const code = 409;
+                    const error = `An account with that ${field} already exists.`;
+                    res.status(code).send({messages: error, fields: field});
+                }
+                if(err.name === 'ValidationError') {
+                    let errors = Object.values(err.errors).map(el => el.message);
+                    let fields = Object.values(err.errors).map(el => el.path);
+                    let code = 400;
+                    if(errors.length > 1) {
+                        const formattedErrors = errors.join(' ');
+                        res.status(code).send({messages: formattedErrors, fields: fields});
+                    } else {
+                        res.status(code).send({messages: errors, fields: fields})
+                    }
+                }
+            }
+        })
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    // const list = await new UserModel(req.body);
+    //     list.save((err,user)=>{
+    //         if(user){
+    //             res.status(201).send({
+    //             list,
+    //             message:"User added"
+    //         });
+    //         }
+    //         else{
+    //             try {
+    //                 if(err.code && err.code == 11000){
+    //                     console.log(Object.keys(err.keyValue).toString())
+    //                     const field = Object.keys(err.keyValue).toString();
+    //                     const code = 409;
+    //                     const error = `An account with that ${field} already exists.`;
+    //                     res.status(code).send({messages: error, fields: field});
+    //                 }
+    //                 if(err.name === 'ValidationError'){
+    //                     let errors = Object.values(err.errors).map(el => el.message);
+    //                     let fields = Object.values(err.errors).map(el => el.path);
+    //                     let code = 400;
+    //                     if(errors.length > 1) {
+    //                         const formattedErrors = errors.join(' ');
+    //                         res.status(code).send({messages: formattedErrors, fields: fields});
+    //                     } else {
+    //                         res.status(code).send({messages: errors, fields: fields})
+    //                     }
+    //                 }
+    //             }
+    //             catch (error) {
+    //                 res.status(500).send('An unknown error occured.');
+    //             }
+    //         }
+    //     });
+});
 
 module.exports = router;
