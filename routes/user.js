bc098fe588f701733983f07275222a0c1828e9ba
Vetrivelan <62693952+vetri09@users.noreply.github.com>
2021-09-22 20:22:12 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const UserModel = require('../models/userModel');
 // const bcrypt = require('bcrypt');
 // const jwt = require('jsonwebtoken');
-// const Imgbb = require('imgbbjs');
-// const imgbb = new Imgbb({
-//   key: process.env.IMGBB_API_TOKEN,
-// });
+const Imgbb = require('imgbbjs');
+const imgbb = new Imgbb({
+  key: process.env.IMGBB_API_TOKEN,
+});
 // get
 // get all users
 router.get('/', async (req, res) => {
@@ -79,5 +79,87 @@ router.get('/:_id', async(req, res) => {
         res.status(500).json(error);
     }
 });
+// put
+// update user
+router.put('/update/:_id', async(req,res)=>{
+    if(req.body._id === req.params._id)
+    {
+        if(req.body.profilePicture)
+        {
+            const imgbbObj = await imgbb.upload(req.body.profilePicture);
+            req.body.profilePicture = (imgbbObj.data.url).toString();
+        }
+        const user = await UserModel.findByIdAndUpdate(req.params._id, {
+            $set:req.body
+        })
+        console.log(req.body)
+        res.status(200).send({
+            user,
+            message:"User updated"
+        })
+    }
+    else{
+        res.status(403).json({messages:"You can updated only your account"})
+    }
+    //     if(req.body.password)
+    //     {
+    //         const hashPass = bcrypt.hash(req.body.password,10);
+    //         req.body.password = (await hashPass).toString();
+    //     }
+});
+// toggle follow and unfollow
+router.put('/:_id/follow', async(req,res) => {
+    if(req.body._id !== req.params._id){
+        try {
+            // the user you wannna follow
+            const user = await UserModel.findById(req.params._id);
+            // you
+            const currentUser = await UserModel.findById(req.body._id);
+            // check if u are not following
+            if(!currentUser.following.includes(req.params._id)){
+                await user.updateOne( { $push: { followers : req.body._id } } );
+                await currentUser.updateOne( { $push: { following : req.params._id } } );
+                res.status(200).json("User has been followed");
+            }
+            // unfollowing the user
+            else{
+                await user.updateOne( { $pull: { followers : req.body._id } } );
+                await currentUser.updateOne( { $pull: { following : req.params._id } } );
+                res.status(200).json("User has been unfollowed");
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    else{
+        res.status(403).json({messages:"You cannot follow yourself"})
+    }
+});
+// delete
+// delete user
+router.delete('/delete/:_id', async(req, res) => {
+    if(req.body._id === req.params._id)
+    {
+        const user = await UserModel.findByIdAndDelete(req.params._id)
+        res.status(200).send({
+            user,
+            message:"User deleted"
+        })
+    }
+    else{
+        res.status(403).json({messages:"You can only delete your account"})
+    }
+});
+// // verify token 
+// const authenticateToken = (req,res,next) => {
+//     const token = req.headers['x-access-token']
+//     if(token === null) return res.sendStatus(401)
+
+//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//         if(err) return res.status(403).send(err)
+//         req.userId = decoded._id;
+//         next()
+//     })
+// };
 
 module.exports = router;
