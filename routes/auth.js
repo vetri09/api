08fc7322b5e2c42a9a06bc7517402f984b334d43
Vetrivelan <vetri0907.vv@gmail.com>
2021-09-23 22:58:57 +0530

    done

--- routes/auth.js
+++ routes/auth.js
@@ -7,49 +7,53 @@ const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 router.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  next()
+  next();
 });
 // user email verification send
 router.post('/verify', async(req,res)=>{
-    const {userName,email} = req.body;
-    console.log(req.body);
-    const emailToken = jwt.sign(
-        {
-            email: email,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: '1d',
-        },
-    );
-    // const url = `http://localhost:3001/auth/confirmtoken/${emailToken}`
-    const url = `https://demooapi.herokuapp.com/auth/confirmtoken/${emailToken}`
+    try {
+        const {userName,email} = req.body;
+        const emailToken = jwt.sign(
+            {
+                email: email,
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: '1d',
+            },
+        );
+        // const url = `http://localhost:3001/auth/confirmtoken/${emailToken}`
+        const url = `https://demooapi.herokuapp.com/auth/confirmtoken/${emailToken}`
 
-    let smptTransport = nodemailer.createTransport({
-        service:"Gmail",
-        port:465,
-        auth:{
-        user: process.env.GMAIL_EMAIL,
-        pass: process.env.GMAIL_PASS
-        }
-  });
+        let smptTransport = nodemailer.createTransport({
+            service:"Gmail",
+            port:465,
+            auth:{
+            user: process.env.GMAIL_EMAIL,
+            pass: process.env.GMAIL_PASS
+            }
+        });
 
-  let mailOptions = {
-    from: process.env.GMAIL_EMAIL,
-    to: email,
-    subject:`Confirm email`,
-    html:`<p>Hey ${ userName }, Please click this link to verify your email: ${ url }</p>`
-  };
+        let mailOptions = {
+            from: process.env.GMAIL_EMAIL,
+            to: email,
+            subject:`Confirm email`,
+            html:`<p>Hey ${ userName }, Please click this link to verify your email: ${ url }</p>`
+        };
 
-  await smptTransport.sendMail(mailOptions,(err,res)=>{
-    if(err){
-        console.log(err);
-    }else{
-        res.send("sucess");
+        await smptTransport.sendMail(mailOptions,(err,res)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.send("sucess");
+            }
+        });
+
+        smptTransport.close();
     }
-  });
-
-  smptTransport.close();
+    catch (error) {
+        res.status(500).send(error);
+    }
 });
 // user email verification receive
 router.get('/confirmtoken/:token', async(req,res)=>{
@@ -99,40 +103,6 @@ router.post('/register', async(req,res)=>{
     } catch (error) {
         res.status(500).send(error);
     }
-    // const list = await new UserModel(req.body);
-    //     list.save((err,user)=>{
-    //         if(user){
-    //             res.status(201).send({
-    //             list,
-    //             message:"User added"
-    //         });
-    //         }
-    //         else{
-    //             try {
-    //                 if(err.code && err.code == 11000){
-    //                     console.log(Object.keys(err.keyValue).toString())
-    //                     const field = Object.keys(err.keyValue).toString();
-    //                     const code = 409;
-    //                     const error = `An account with that ${field} already exists.`;
-    //                     res.status(code).send({messages: error, fields: field});
-    //                 }
-    //                 if(err.name === 'ValidationError'){
-    //                     let errors = Object.values(err.errors).map(el => el.message);
-    //                     let fields = Object.values(err.errors).map(el => el.path);
-    //                     let code = 400;
-    //                     if(errors.length > 1) {
-    //                         const formattedErrors = errors.join(' ');
-    //                         res.status(code).send({messages: formattedErrors, fields: fields});
-    //                     } else {
-    //                         res.status(code).send({messages: errors, fields: fields})
-    //                     }
-    //                 }
-    //             }
-    //             catch (error) {
-    //                 res.status(500).send('An unknown error occured.');
-    //             }
-    //         }
-    //     });
 });
 // user login
 router.post('/login', async(req,res)=>{
@@ -162,6 +132,6 @@ router.post('/login', async(req,res)=>{
         user,
         messages:"Login success"
     })
-})
+});
 
 module.exports = router;
